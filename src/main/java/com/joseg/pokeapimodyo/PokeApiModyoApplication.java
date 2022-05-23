2d246478daf33899b845a32be46b13d66c3792b2
Jose Guzman <2guzmanjose@gmail.com>
2022-05-23 10:36:05 -0400

    base commit

---
+++ src/main/java/com/joseg/pokeapimodyo/PokeApiModyoApplication.java
@@ -0,0 +1,13 @@
+package com.joseg.pokeapimodyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiModyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApiModyoApplication.class, args);
+	}
+
+}
